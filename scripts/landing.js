a9252b92da404ee49822e4ca55191e67ad52a3e1
Nicolas <nrmunley@gmail.com>
2017-11-01 22:15:47 -0700

    Assignment 21 DOM animation edit

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,9 +4,9 @@ var points = document.getElementsByClassName('point');
 
 var revealPoint = function(index) {
     points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0) rotate(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0) rotate(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0) rotate(0)";
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 };
     for (var i = 0; i < points.length; i++){
     revealPoint(i);
