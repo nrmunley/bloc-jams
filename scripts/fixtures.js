5080155a13369ba4bfec57d4e74f7f04c87ed4c6
Nicolas <nrmunley@gmail.com>
2017-11-24 11:43:34 -0800

    Checkpoint 31 play/pause: completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+// Example album
+ var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+ // Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+// Another Example Album
+var albumMarconiII = {
+    title: 'The Telephone BOOOOOTH (Halloween v1)',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1910',
+    albumArtUrl: 'assets/images/album_covers/02.png',
+    songs: [
+        { title: 'Are you there? Operator?', duration: '1:01' },
+        { title: 'Ring, ring... Click', duration: '5:01' },
+        { title: 'Goblins in your pocket', duration: '3:21'},
+        { title: 'HELP! Can you hear me?', duration: '3:14' },
+        { title: 'No Name No. 1', duration: '2:15'}
+    ]
+};
