87a4e10a6d800c4ac1bbb30bf1dc6107706313c5
Nicolas <nrmunley@gmail.com>
2017-11-27 19:21:04 -0800

    Assignment 31 next and previous added setSong

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,8 @@
+var setSong = function(songNumber) {
+  currentlyPlayingSongNumber = parseInt(songNumber);
+  currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+};
+
 var createSongRow = function(songNumber, songName, songLength){
   var template =
     '<tr class="album-view-song-item">'
@@ -18,8 +23,7 @@ var createSongRow = function(songNumber, songName, songLength){
     }
       if (currentlyPlayingSongNumber !== songNumber) {
       $(this).html(pauseButtonTemplate);
-      currentlyPlayingSongNumber = songNumber;
-      currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+      setSong(songNumber);
       updatePlayerBarSong();
     } else if (currentlyPlayingSongNumber === songNumber) {
       $(this).html(playButtonTemplate);
